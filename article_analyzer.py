cc58bd068e4b63560903cf192268dfbe43c13a04
chrisneuf <christian.neufeld@alumni.ubc.ca>
2019-06-15 12:20:30 -0700

    changed from newsAPI to google search for finding related articles

--- article_analyzer.py
+++ article_analyzer.py
@@ -1,76 +1,52 @@
-from newsapi import NewsApiClient
+
 from aylienapiclient import textapi
+from googlesearch import search
 import sys
 import textcleaner as tc
+import tldextract
 
 
 #Setup API Keys
-news_api = NewsApiClient(api_key='e65ead4cf44c49efa7eeac4a9fc4aa7c')
 extraction_api = textapi.Client("a65d9a56", "1946036c4ab9e433a7f87a45405f6235")
 
 
 
-#Get article from Url
-url = firstarg=sys.argv[1]
-article = extraction_api.Extract({'url': url})
-title = article['title']
-print()
-print("Url article title")
-print(article['title'])
-print()
 
 
-
-#Find Related Articles
-# Early stages currently only does one word queries
-headlines = []
-for word in title:
+def main():
         
-        # /v2/top-headlines
-        top_headlines = news_api.get_top_headlines(q=word,
-                                                language='en',
-                                                )
+        url = firstarg=sys.argv[1]
+        root = get_root(url)
+        article = extraction_api.Extract({'url': url})
+        title = article['title']
 
-        for headline in top_headlines['articles']:
-                if headline not in headlines:
-                        headlines.append(headline)
-
-# print(titles[:3])
-# data = tc.document(titles)
-# titles = data.remove_stpwrds()
-# print(titles)
-# print()
-# print()
-# print()
+        
+        print("\nUrl article title")
+        print(f"{article['title']}\n")
+        
+        # Find Related articles
+        related_articles = get_related_articles(title, root, 5)
 
 
-def similar(s1, s2):
-    sameWords = set.intersection(set(s1.split(" ")), set(s2.split(" ")))
-    return len(sameWords)
-def getKey(item):
-        return item[0]
+# Returns the root of url
+def get_root(url):
+      ext = tldextract.extract(url)
+      return ext[1]
 
-currentSimilar = 0
+# Returns a list of  articles related to title less than max size
+def get_related_articles(title, root, max):
 
-rank = []
+        related_articles = []
+        for url in search(title, stop=max):
+                if root != get_root(url):
+                        article = extraction_api.Extract({'url': url})
+                        related_articles.append(article)
+                        print(article['title'])
+                        print(f"{url}\n")
+        return related_articles
 
-for headline in headlines:
-        similiarity = similar(title.lower(), headline['title'].lower())
-        rank.append([similiarity,headline])
-        # print()
-        # print(similiarity)
-        # print(headline['title'])
-        # print()
-
-rank = sorted(rank, key=getKey, reverse = True)
-print("Similar Articles")
-for i in range(5):
-        print()
-        print(rank[i][1]['title'])
-        print(rank[i][1]['url'])
-        print()
-
-
+if __name__ == "__main__":
+        main() 
 
 #Compare Articles 
 
